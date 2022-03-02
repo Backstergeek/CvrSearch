996fa03bbbada865e3fe3f69d26b04dddc2e4847
Andreas <andreas-berg@outlook.dk>
2022-03-02 15:37:58 +0100

    Removed button search button added enter search

--- CVRSearch/Components/CvrSearch.tsx
+++ CVRSearch/Components/CvrSearch.tsx
@@ -1,76 +1,97 @@
-import { Stack, TextField, IStackTokens, PrimaryButton} from '@fluentui/react';
+import { PrimaryButton} from '@fluentui/react/lib/Button';
+import { Stack, StackItem } from '@fluentui/react/lib/Stack';
+import { TextField } from '@fluentui/react/lib/TextField';
 import * as React from 'react';
-import { ICvrProps, ICVRRootObject } from "../Interfaces/CVRInterface";
+import { CVRRootObject, ICvrProps } from "../Interfaces/CVRInterface";
 
 
 
 export const CvrSearch: React.FC<ICvrProps> = (props) => {
 
-    const [cvrObj, setCvrObj] = React.useState<ICVRRootObject>();
+    const [cvrFinalObj, setCvrObj] = React.useState<CVRRootObject[]>();
     const [input, setinput] = React.useState<string>("");
-    const [res, setRes] = React.useState<string>("");
     const [show, setShow] = React.useState<boolean>(false);
+    const [loading, setLoading] = React.useState<boolean>(false);
+    const [loadingTxt, setLoadingTxt] = React.useState<string>("");
 
+  
     function getCvrData(): void {
-        setRes("Searching...")
+
+        setLoading(true);
+        setLoadingTxt("Søger....")
         let searchString: string = input;
-        let headers = new Headers({
-            "user-Agent" : "Donkey Power - CRM Help when creating account - Andreas Helbo Berg - ahb@donkeypower.dk"
-        });
+        let isName: boolean = false;
 
-        let queryString = `//cvrapi.dk/api?search=${searchString}&country=${props.countryCode}`;
+        if (isNaN(Number(searchString))) {
+          isName = true
+        } 
 
-        fetch(queryString, {headers}).then(res => res.json()).then((r: ICVRRootObject) => {
-            setRes(`Name: ${r?.name}\n CVR: ${r?.vat}\n Address: ${r?.address}`)
-            setCvrObj(r)
-            setShow(true);
+        let queryString = `https://prod-197.westeurope.logic.azure.com/workflows/e442ab56ea5f4ef9b9735801710e33bc/triggers/manual/paths/invoke/cvr_number/${searchString}/isText/${isName}?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=twqBcn8feKqds9ixm9yv4m0ZXJ0ZwvlCxtUhBMnx86c`
+        
+        var requestOptions: RequestInit = {
+            method: 'GET',
+        }
+
+
+
+        fetch(queryString,requestOptions).then(res => res.json()).then((r: CVRRootObject[]) => {
+
+          setLoading(false);
+          setCvrObj(r)
+          setShow(true);
+          
+        }).catch((e) => {
+          setLoadingTxt("Søgningen gav ingen resultater")
         })
     }
 
-    function setData(): void {
-        if (props.outPutFunction)
-        props.outPutFunction(
-            cvrObj!.name,
-            cvrObj!.vat.toString(),
-            cvrObj!.address,
-            cvrObj!.city,
-            cvrObj!.zipcode,
-            cvrObj!.startdate,
-            cvrObj!.email,
-            cvrObj!.phone,
-        )
-    }
-
-    
-
-
-    const stackTokens: IStackTokens = {childrenGap: 5};
-
-
     return (
-        <Stack tokens={stackTokens}>
+        <Stack>
             <Stack horizontal>
-                <Stack.Item align='start'> 
-                    <TextField 
-                        onChange={(e, val) => {setinput(val!)}} 
-                        placeholder='Write CVR no or company name'
-                    />
-                </Stack.Item>
-                <Stack.Item align='start'>
-                    <PrimaryButton 
-                        text='Search' 
-                        onClick={getCvrData}
-                    />
-                </Stack.Item>   
+              <StackItem grow={4}>
+                <TextField 
+                    onChange={(e, val) => {setinput(val!)}} 
+                    placeholder={props.context.resources.getString("TextFieldPlaceHolder")}
+                    onBlur={() => {setTimeout(() => {setShow(false)},100)}}
+                    onFocus={() => {setTimeout(() => {setShow(true)},100)}}
+                    onKeyDown={(event) => {if(event.key === "Enter") {
+                      getCvrData()
+                    }}}
+                />
+              </StackItem>
+              {/* <StackItem>
+                <PrimaryButton 
+                    text={props.context.resources.getString("ButtonPlaceHolder")} 
+                    onClick={getCvrData}
+                />
+              </StackItem>                   */}
             </Stack>
-            <Stack horizontal style={{display: (show ? 'block': 'none')}}>
-                <Stack.Item align='start'>
-                    <TextField multiline rows={3} value={res}/>
-                </Stack.Item>
-                <Stack.Item align='start'>
-                    <PrimaryButton onClick={setData} text='Set data'></PrimaryButton>
-                </Stack.Item>      
-            </Stack>
+            {loading ? <Stack><StackItem>{loadingTxt}</StackItem></Stack> :
+            <Stack style={{display: (show? 'block': 'none')}}>
+              <ul id="CVRlist">
+                  {
+                    cvrFinalObj?.map(i => (
+                      <li key={i.cvrNummer} 
+                      onClick={() => {
+                        if(props.outPutFunction)
+                        props.outPutFunction(
+                          i.virksomhedMetadata.nyesteNavn.navn,
+                          i.cvrNummer.toString(),
+                          `${i.virksomhedMetadata.nyesteBeliggenhedsadresse.vejnavn} ${i.virksomhedMetadata.nyesteBeliggenhedsadresse.husnummerFra}`,
+                          i.virksomhedMetadata.nyesteBeliggenhedsadresse.postdistrikt,
+                          i.virksomhedMetadata.nyesteBeliggenhedsadresse.postnummer.toString(),
+                          "",
+                          "",
+                          ""
+                        )
+                        setTimeout(() => {setShow(false)},200)
+                      }}>
+                        {i.virksomhedMetadata.nyesteNavn.navn} - {i.cvrNummer}
+                      </li>
+                    ))
+                  }
+              </ul>
+            </Stack>}
         </Stack>
     )
 }
